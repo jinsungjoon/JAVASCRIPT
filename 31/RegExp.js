23130f1a0453421e08a851c2b8b1e1a8acbfa717
jinsungjoon <juney0623@gmail.com>
2022-05-29 23:12:16 +0900

    2022-05-29

--- 31/RegExp.js
+++ 31/RegExp.js
@@ -169,4 +169,137 @@ console.log(target.match(regExp));
 regExp = /A+/g;
 console.log(target.match(regExp));
 
-// ?는 앞선 패턴이 최대 한 번(0번 포함) 이상 반복되는 문자열을 의미한다.즉,?는 {0,1}과 같다. 다음 예제의 
+// ?는 앞선 패턴이 최대 한 번(0번 포함) 이상 반복되는 문자열을 의미한다.
+// 즉,?는 {0,1}과 같다. 다음 예제의 
+target = 'color colour';
+regExp = /colou?r/g;
+console.log(target.match(regExp));
+
+
+// 31.5.4 OR 검색
+// |은 or의 의미를 갖는다. 다음예제의 /A|B/g;
+target = 'A AA B BB Aa Bb';
+// 'A' 또는 'B'를 전역 검색한다.
+regExp = /A|B/g;
+console.log(target.match(regExp));
+
+// 분해되지 않은 단어 레벨로 검색하기 위해서는 +를 함께 사용한다.
+regExp = /A+|B+/g;
+console.log(target.match(regExp));
+
+// 위 예제는 패턴을 or로 한 번 이상 반복하는 것인데 이를 간단히 표현하면 다음과 같다.
+// []내의 문자는 or로 동작한다.그 뒤에 +를 사용함면 앞선 패턴을 한 번 이상 반복한다.
+regExp = /[AB]+/g;
+console.log(target.match(regExp));
+
+// 범위를 지정하려면 []내에 -를 사용한다.다음 예제의 경우 대문자 알파벳을 검색한다.
+regExp = /[A-Z]+/g;
+target = 'A AA BB C CC ZZ Aa Bb';
+console.log(target.match(regExp));
+
+regExp = /[A-Z]+/g;
+target = 'A AA AAA BB C CC ZZ Aa Bb';
+console.log(target.match(regExp));
+
+// 대소문자를 구별하지 않고 알파벳을 검색하는 방법은 다음과 같다
+target = 'AA BB Aa Bb 12';
+regExp = /[A-Za-z]+/g;
+console.log(target.match(regExp));
+// 'A' - 'Z' 또는 'a' - 'z'가 한 번 이상 반복되는 문자열을 전역
+regExp = /[A-Z]+/gi;
+console.log(target.match(regExp));
+
+//숫자를 검색하는 방법은 다음과 같다.
+target = 'AA BB 12,345';
+regExp = /[0-9]+/g;
+console.log(target.match(regExp));
+
+regExp = /[0-9,]+/g;
+console.log(target.match(regExp));
+
+// 위 예제를 간단히 표현하면 다음과 같다.\d는 숫자를 의미한다.
+// 즉, \d는 [0-9]와 같다.\D는 \d와 반대로 동작한다.
+// 즉, \D는 숫자가 아닌 문자를 의미한다. 
+
+
+// '0' - '9'가 아닌 문자(숫자가 아닌 문자)또는 ','가 한 번 이상 반복되는 문자열을 전역 검색한다.
+regExp = /[\D,]+/g;
+console.log(target.match(regExp));
+
+// \w는 알파벳, 숫자 ,언더스코어를 의미한다.즉,\w는 [A=Za-z0-9_]와 같다.
+// \W는 알파벳, 숫자 ,언더스코어가 아닌 문자를 의미한다.
+
+target = 'AA BB 12,345 _$&&';
+regExp = /[\w,]+/g;
+console.log(target.match(regExp));
+
+
+regExp = /[\W,]+/g;
+console.log(target.match(regExp));
+
+// 31.5.5 NOT 검색
+// [...]내의 ^은 not의 의미를 갖는다. 예를 들어,[^0-9]는 숫자를 제외한 문자를 의미한다.
+// 따라서[0-9]와 같은 의미의 \d와 반대로 동작하는 \D는 [^0-9]와 같고,[A-Za-z0-9_]와 같은
+// 의미의 \w와 반대로 동작하는 \W는 [^A-Za-z0-9_]와 같다.
+
+target = 'AA BB 12 Aa Bb';
+//숫자를 제외한 문자열을 적연 검색한다.
+regExp = /[^0-9]+/g;
+console.log(target.match(regExp));
+
+// 31.5.6 시작 위치로 검색
+target = 'https://poiemaweb.com';
+regExp = /^https/;
+console.log(regExp.test(target));
+
+// 31.5.7  마지막 위치로 검색
+// $는 문자열의 마지막을 의미한다.
+regExp = /com$/;
+console.log(regExp.test(target));
+
+// 31.6  자주 사용하는 정규표현식
+// 31.6.1 특정 단어로 시작하는지 검사
+//다음 예제는 검색 대상 문자열이 'http://' 또는 'https://'로 시작하는지 검사한다.
+// [...] 바깥의 ^은 문자열의 시작을 의미하고,?은 앞선 패턴(다음 예제의 경우 's')이 최대 한번(0번 포함)
+// 이상 반복되는지를 의미한다. 다시말해 검색대상 문자열에 앞선 패턴('s')이 있어도 없어도 매치된다.
+console.log(/^https?:\/\//.test(target));
+
+// 31.6.2 특정 단어로 끝나는지 검사
+// 다음 예제는 검색 대상이 문자열이 'html'로 끝나는지 검사한다. '$'는 문자열의 마지막을 의미한다.
+let filename = 'index.html';
+console.log(/html$/.test(filename));
+
+
+// 31.6.3 숫자로만 이루어진 문자열인지 검사
+// 다음 예제는 검색 대상 문자열이 숫자로만 이루어진 문자열인지 검사한다.
+// [...] 바깥의 ^은 문자열의 시작을,$는 문자열의 마지막을 의미한다.즉,처음과
+// 끝이 숫자이고 최소 한 번 이상 반복되는 문자열과 매치한다.
+target = '12345';
+console.log(/^\d+$/.test(target));
+
+// 31.6.4 하나 이상의 공백으로 시작하는지 검사
+// 다음 예제는 검색 대상 문자열이 하나 이상의 공백으로 시작하는지 검사한다.
+// \s는 여러가지 공백 문자(스페이스,탭 등)를 의미한다.즉, \s는 [\t\r\n\v\f]와 같은 의미다.
+target = ' Hi!';
+console.log(/^[\s]+/.test(target));
+
+// 31.6.5 아이디로 사용 가능한지 검사
+// 다음 예제는 검색 대상 문자열이 알파벳 대소문자 또는 숫자로 시작하고 끝나며 4~10자리인지 검사한다.
+// {4,10}은 앞선 패턴(알파벳 대소문자 또는 숫자)이 최소4번,최대10번 반복되는 문자열을 의미한다.
+// 즉,4~10자리로 이루어진 알파벳 대소문자 또는 숫자를 의미한다.
+let id = 'abc123';
+console.log(/^[A-Za-z0-9]{4,10}$/.test(id));
+
+// 31.6.7 핸드폰 번호 형식에 맞는지 검사
+// 다음 예제는 검색 대상 문자열이 핸드폰 번호 형식에 맞는지 검사한다.
+let cellphone = '010-1234-5678';
+console.log(/^\d{3}-\d{3,4}-\d{4}$/.test(cellphone));
+
+let float = '1234.3';
+console.log(/^\d{0,5}.\d{0,3}$/.test(float));
+
+// 31.6.8 특수 문자 포함 여부 검사
+// 다음 예제는 검색 대상 문자열에 특수 문자가 포함되어 있는지 검사한다.
+// 특수 문자는 A-Za-z0-9 이외의 문자다.
+target = 'abc#123';
+console.log(target.replace(/[^A-Za-z0-9]/gi,''));
